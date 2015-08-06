39ca318652cfdbdf24c007ab8baffbf1bc9f4a96
Herbert Lange <sonofchaos@gmx.de>
2015-08-06 12:18:19 +0200

    Added first tech demo version to see how the irc module is working

---
+++ Main.hs
@@ -0,0 +1,26 @@
+import Network.SimpleIRC
+import Data.Maybe
+import qualified Data.ByteString.Char8 as B
+
+onPrivMsg :: EventFunc
+onPrivMsg s m =
+    do
+      putStr "Privmsg: "
+      putStrLn $ show m
+onJoinMsg :: EventFunc
+onJoinMsg s m =
+    do
+      putStr "Joinmsg: "
+      putStrLn $ show m
+                 
+events = [(Privmsg onPrivMsg), (Join onJoinMsg)]
+           
+freenode = (mkDefaultConfig "irc.freenode.net" "annaisnotabot")
+           {
+             cChannels = ["#botenannastestchannel"], -- Channels to join on connect
+             cEvents   = events -- Events to bind
+           }                                                                                                                               
+
+main =
+    do
+      connect freenode False True
