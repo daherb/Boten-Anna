37e2be2ec3eea2044e7e34e0ae1d3870250aa478
Herbert Lange <sonofchaos@gmx.de>
2015-08-07 11:56:35 +0200

    added reaction to direct mentions

--- Main.hs
+++ Main.hs
@@ -2,17 +2,26 @@ import Network.SimpleIRC
 import Data.Maybe
 import qualified Data.ByteString.Char8 as B
 
+botname = "anaisnotabot"
+channelname = "#esslli2015"
+              
 onPrivMsg :: EventFunc
-onPrivMsg s m =
-    do
-      putStr "Privmsg: "
-      putStrLn $ show m
+onPrivMsg s m
+    |  B.isPrefixOf (B.pack botname) msg = do
+      sendMsg s chan (B.append (B.pack "What do you want by saying: \"") (B.append (B.drop ((length botname) + 2) msg) (B.pack "\"?")))
+    | True =
+        do
+          putStr "Privmsg: "
+          putStrLn $ show m
+    where chan = fromJust $ mChan m
+          msg = mMsg m
 onJoinMsg :: EventFunc
 onJoinMsg s m =
     do
       putStr "Joinmsg: "
       putStrLn $ show m
-      sendCmd s (MMode (B.pack (head (cChannels freenode))) (B.pack "+o") (mNick m))
+      sendCmd s (MMode chan (B.pack "+o") (mNick m))
+    where chan = fromJust $ mChan m
                  
 events = [(Privmsg onPrivMsg), (Join onJoinMsg)]
            
