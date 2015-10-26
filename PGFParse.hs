7ede0e0415d5e36d9f756b572da620dce2530ee9
Herbert Lange <sonofchaos@gmx.de>
2015-10-26 16:33:07 +0100

    New interface function and now returns all skipped prefix words, the bracketed parse and the ignored suffix words

--- PGFParse.hs
+++ PGFParse.hs
@@ -1,33 +1,44 @@
-module PGFParse (parseLoop) where
+module PGFParse (parseWithPGF) where
 import PGF
 import Data.Maybe
 import Control.Exception
-    
+
 handleOutputs (_,b) = b
-                      
-parseLoop l pgf =
+
+parseLoop prel l pgf =
   let
-    parses = (\p -> case p of (_ , b) -> b) $ parse_ pgf (head $ languages pgf) (startCat pgf) Nothing l
-    cat = (\x -> case x of Bracket c _ _ _ _ _ -> c) parses
+    parseRes = parse_ pgf (head $ languages pgf) (startCat pgf) Nothing l
+    brackets = snd parseRes
+    cat = (\x -> case x of Bracket c _ _ _ _ _ -> c) brackets
+    pos = (\x -> case x of ParseFailed i -> i - 1
+                           _ -> length $ words l) $ fst parseRes
+    rest = unwords $ drop pos $ words l  
   in
     if showCId cat == "_" then
         if l == "" then
             Nothing
         else
-            parseLoop (unwords $ tail $ words l) pgf
+          let
+            ws = words l
+          in
+            parseLoop (prel ++ " " ++ head ws) (unwords $ tail $ ws) pgf
     else
         if (showCId cat) /= (showType [] $ startCat pgf) then 
             Nothing
         else
-            Just parses --(maximum parses)
+            Just (prel,brackets,rest) --(maximum parses)
 
+parseWithPGF l pgf =
+  parseLoop "" l pgf
+  
 readLoop pgf =
     do
       l <- getLine
-      catch (putStrLn $ showBracketedString $ fromJust $ parseLoop l pgf) (\e -> let s = show(e :: SomeException) in putStrLn $ "Panic: " ++ s)
+      let (preB,brackets,postB) = fromJust $ parseLoop "" l pgf
+      catch  (putStrLn $ "Pre: " ++ preB ++ " Brackets: " ++ (showBracketedString $ brackets) ++ " Post: " ++ postB) (\e -> let s = show(e :: SomeException) in putStrLn $ "Panic: " ++ s)
       readLoop pgf
 main =
     do
-      pgf <- readPGF "partial.pgf"
+      pgf <- readPGF "Anna.pgf"
       readLoop pgf
       
