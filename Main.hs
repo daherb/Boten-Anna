2aa0cb7ec7a4dd67550fdcce3b100b93a45e9d7f
Herbert Lange <sonofchaos@gmx.de>
2015-10-26 20:46:53 +0100

    changed old string matching to new gf parsing

--- Main.hs
+++ Main.hs
@@ -1,28 +1,51 @@
 import Network.SimpleIRC
 import Data.Maybe
+import PGFParse
+import PGF
+import Data.Char
 import qualified Data.ByteString.Char8 as B
 
-botnick = "annaärinteenbot"
-channelname = "#esslli2015"
-              
+botnick = "annaaerinteenbot"
+channelname = "#botenannatest"
+
+pgf = readPGF "Anna.pgf"
+
+
+sendResponse s m pre parsed post
+  | findInBracketed "please" parsed && findInBracketed "de-op" parsed =
+    let
+      ws = words post
+    in
+      if head ws == "me" then
+        sendCmd s (MMode (B.pack channelname) (B.pack "-o") (mNick m))
+      else
+        sendCmd s (MMode (B.pack channelname) (B.pack "-o") (Just $ B.pack $ head ws))
+  | findInBracketed "please" parsed && findInBracketed "op" parsed =
+    let
+      ws = words post
+    in
+      if head ws == "me" then
+        sendCmd s (MMode (B.pack channelname) (B.pack "+o") (mNick m))
+      else
+        sendCmd s (MMode (B.pack channelname) (B.pack "+o") (Just $ B.pack $ head ws))
+  | findInBracketed "op" parsed || findInBracketed "deop" parsed =
+      sendMsg s (fromJust $ mChan m) (B.pack "You have to be more polite if I should help you")
+  | findInBracketed "anna" parsed || findInBracketed "annas" parsed =
+      sendMsg s (fromJust $ mChan m) (B.pack "Are you talking about me?")
+  | findInBracketed "bot" parsed || findInBracketed "bots" parsed =
+      sendMsg s (fromJust $ mChan m) (B.pack "I am not a bot!")
+  | (head $ words pre) == (botnick ++ ":") =
+      sendMsg s (fromJust $ mChan m) (B.pack $ "What do you want to accomplish by saying: \"" ++ ( unwords $ tail $ words pre ) ++ "\"")
+  | otherwise = return ()
 onPrivMsg :: EventFunc
-onPrivMsg s m
-    -- Bound by laws of robotics https://en.wikipedia.org/wiki/Robotic_laws
-    | fst (B.breakSubstring (B.pack "kill") msg) /= msg || fst (B.breakSubstring (B.pack "hurt") msg) /= msg || fst (B.breakSubstring (B.pack "order") msg) /= msg  = do
-      sendMsg s chan (B.pack "I am bound by Asimov's laws")
-    -- other rules
-    |  B.isPrefixOf (B.pack (botnick ++ ":")) msg = do
-      sendMsg s chan (B.append (B.pack "What do you want to accomplish by saying: \"") (B.append (B.drop ((length botnick) + 2) msg) (B.pack "\"?")))
-    | fst (B.breakSubstring (B.pack " bot") msg) /= msg = do
-      sendMsg s chan (B.pack "I am not a bot")
-    | (fst (B.breakSubstring (B.pack " anna") msg) /= msg) && ((fromJust $ mNick m) /= (B.pack botnick)) = do
-      sendMsg s chan (B.pack "Are you talking about me?")
-    | True =
-        do
-          putStr "Privmsg: "
-          putStrLn $ show m
-    where chan = fromJust $ mChan m
-          msg = mMsg m
+onPrivMsg s m =
+  do
+    mpgf <- pgf
+    let text = B.unpack $ mMsg m
+    case parseWithPGF (map toLower text) mpgf of
+      Nothing -> sendResponse s m (map toLower text) [] ""
+      Just (pre,parsed,post) -> sendResponse s m pre [parsed] post
+--    putStrLn $ show m
                 
 onJoinMsg :: EventFunc
 onJoinMsg s m =
@@ -31,7 +54,7 @@ onJoinMsg s m =
       putStrLn $ show m
       sendCmd s (MMode chan (B.pack "+o") (mNick m))
     where chan = B.pack channelname
-                 
+
 events = [(Privmsg onPrivMsg), (Join onJoinMsg)]
            
 freenode = (mkDefaultConfig "irc.freenode.net" botnick)
