cfc997e91488ad4aaaad55df77afd17d9eee4696
Herbert Lange <sonofchaos@gmx.de>
2016-03-10 19:03:32 +0100

    Cleanup and comments

--- Main.hs
+++ Main.hs
@@ -10,30 +10,36 @@ import qualified Data.Set as S
 import System.IO
 import Control.Monad
 
+-- Global config constants
 version = "0.3"
 botNick = "annaisnotabot"
 channelNames = ["#botenannatest","#botenannatest1"]
 debugMsg = False
 logging = True
 logName = "boten-anna.log"
+pgf = readPGF "Anna.pgf"
 
+-- Data type for the nick list
 type NickList = ([(B.ByteString,S.Set B.ByteString)])
 
+-- Data type for the message list
 data Message = Envelop {
   channel :: String,
   from :: String,
   to :: String,
   message :: String };
   
-pgf = readPGF "Anna.pgf"
-
+-- Normalize strings
 normalize :: String -> String
+-- Replace punctuation by spaces and make everything lower case
 normalize = map (\c -> if elem c ".,!?" then ' ' else toLower c)
 
+-- Send a reply to a message
 sendResponse :: MIrc -> IrcMessage -> String -> [Expr] -> String -> IORef [Message] -> IO ()
 sendResponse s m pre parsed post iomessages =
     do
       grammar <- pgf
+      -- linearize the parsed query inro a response
       let response = linearize grammar (mkCId "EngR") $ head parsed
       return ()
   -- | isPrefixOf ( botnick ++ ":" ) pre && findInAbsTrees "tell" parsed && (length ( words post ) >= 2) =
@@ -92,70 +98,95 @@ sendResponse s m pre parsed post iomessages =
   where chan = if isJust (mChan m) then fromJust (mChan m) else B.pack ""
         nick = if isJust (mNick m) then fromJust (mNick m) else B.pack ""
 
-onPrivMsg :: IORef ([Message]) -> IORef NickList -> EventFunc
-onPrivMsg iomessages ionicks s m =
-  do
-    mpgf <- pgf
-    messages <- readIORef iomessages
-    let text = B.unpack $ mMsg m
-    remaining <- printMessages messages s nick chan
-    writeIORef iomessages remaining
-    putStrLn ("###" ++ text)
-    case parseWithPGF (normalize text) mpgf (mkCId "EngQ") [mkType [] (mkCId "Placeholder") []] of
-      Left res -> do
-        putStrLn $ show res
-        sendResponse s m text [] "" iomessages
-      Right (pre,parsed,post) -> do
-        putStrLn $ "Pre: " ++ pre ++ " Parse trees: " ++ (show parsed) ++ " Post: " ++ post ++ " EOL"
-        sendResponse s m pre parsed post iomessages
-  where chan = if isJust (mChan m) then fromJust (mChan m) else B.pack ""
-        nick = if isJust (mNick m) then fromJust (mNick m) else B.pack ""
-
+-- Forwards messages to users on user actions
 printMessages :: [Message] -> MIrc -> B.ByteString -> B.ByteString -> IO [Message]
+-- No messages left, do nothing
 printMessages [] _ _ _ = do return []
-printMessages ((msg@(Envelop {channel = c, from = f,to = t, message = m})):ms) s nick chan = 
+-- Go through all messages, see if you can find the nick as a recipient
+printMessages ((msg@(Envelop {channel = c, from = f,to = t, message = m})):ms) s nick chan =
+  -- If so forward message to him and inform the sender about the sucessful delivery
   if t == (normalize $ B.unpack nick) && c == B.unpack chan then
     do
       sendMsg s chan $ B.pack ((B.unpack nick) ++ ": " ++ f ++ " wants me to tell you " ++ m)
       sendMsg s chan $ B.pack (f ++ ": Transmitted your message to " ++ (B.unpack nick))
       rest <- printMessages ms s nick chan
       return rest
+   -- Otherwise continue looking
    else
      do
        rest <- printMessages ms s nick chan
        return (msg:rest)
-  
+       
+-- Callback on general chat messages
+onPrivMsg :: IORef ([Message]) -> IORef NickList -> EventFunc
+onPrivMsg iomessages ionicks s m =
+  do
+    -- Get constants and parameters
+    mpgf <- pgf
+    messages <- readIORef iomessages
+    -- Get message text
+    let text = B.unpack $ mMsg m
+    -- Forward messages and save the remaining ones again
+    remaining <- printMessages messages s nick chan
+    writeIORef iomessages remaining
+    putStrLn ("###" ++ text)
+    -- Try to parse
+    case parseWithPGF (normalize text) mpgf (mkCId "EngQ") [mkType [] (mkCId "Placeholder") []] of
+      -- No success
+      Left res -> do
+        putStrLn $ show res
+        -- Try to generate a response with the whole unparsed message in the pre parameter
+        sendResponse s m text [] "" iomessages
+      -- Parse successful
+      Right (pre,parsed,post) -> do
+        putStrLn $ "Pre: " ++ pre ++ " Parse trees: " ++ (show parsed) ++ " Post: " ++ post ++ " EOL"
+        -- Try to generate a response with the parse tree and a possible pre and post context
+        sendResponse s m pre parsed post iomessages
+  where chan = if isJust (mChan m) then fromJust (mChan m) else B.pack ""
+        nick = if isJust (mNick m) then fromJust (mNick m) else B.pack ""
+
+-- Callback when a user joins
 onJoinMsg :: IORef ([Message]) -> IORef NickList -> EventFunc
 onJoinMsg iomessages ionicklist s m =
   do
+    -- Get constants and parameters
     messages <- readIORef iomessages
     nickList <- readIORef ionicklist
-    if nick == (B.pack botNick) then
+    -- Check if it our own JOIN
+    if (nick == B.pack botNick) then
+      -- Yes -> Get the nicks in the channel
       sendRaw s (B.pack "NAMES channel")
     else
+      -- No -> try to give operator rights to user
       sendCmd s (MMode chan (B.pack "+o") (Just nick))
+    -- Forward messages and save the remaining ones again
     remaining <- printMessages messages s nick chan
     writeIORef iomessages remaining
+    -- Update nick list
     let newNickList = map (\(c,ns) -> if c == chan then (c, S.insert nick ns) else (c,ns)) nickList
     writeIORef ionicklist newNickList
     putStrLn $ show newNickList
     where chan = if isJust (mChan m) then fromJust (mChan m) else (mMsg m)
           nick = if isJust (mNick m) then fromJust (mNick m) else B.pack ""
-    
 
+-- Callback when the bot is invites
 onInviteMsg :: IORef NickList -> EventFunc
 onInviteMsg ionicklist s m =
     do
+      -- Join the channel we are invited to  
       sendCmd s (MJoin (mMsg m) Nothing)
+      -- Add channel to nick list
       nickList <- readIORef ionicklist
       let newNickList = if not (isNothing $ find (\(c,_) -> c == chan) nickList) then ((chan, S.empty):nickList) else nickList
       writeIORef ionicklist newNickList
     where chan = if isJust (mChan m) then fromJust (mChan m) else (mMsg m)
           nick = if isJust (mNick m) then fromJust (mNick m) else B.pack ""
 
+-- Callback when a user leaves channel
 onPartMsg :: IORef NickList -> EventFunc 
 onPartMsg ionicklist s m =
   do
+    -- Update nick list
     nickList <- readIORef ionicklist
     let newNickList = map (\(c,ns) -> if c == chan then (c, ns S.\\ (S.singleton nick)) else (c,ns)) nickList
     writeIORef ionicklist newNickList
@@ -163,35 +194,48 @@ onPartMsg ionicklist s m =
     where chan = if isJust (mChan m) then fromJust (mChan m) else (mMsg m)
           nick = if isJust (mNick m) then fromJust (mNick m) else B.pack ""
 
--- >> :rajaniemi.freenode.net 353 annaisnotabot @ #botenannatest :annaisnotabot @daherb
+-- Callback on any raw IRC message
 onRawMsg :: IORef Handle -> IORef NickList -> EventFunc
 onRawMsg iohandle ionicklist s m =
   do
+    -- Get constants and parameters
     handle <- readIORef iohandle
     nickList <- readIORef ionicklist
+    -- If enabled write the message to the log
     when logging $ hPutStrLn handle $ show m
+    -- If message is of code 353 see RFC2812 RPL_NAMREPLY
     if ((B.isInfixOf . B.pack) " 353 " $ mMsg m) then
-      -- Remove the channel name, the space and the colon and then split at spaces
       do
+        -- Remove the channel name, the space and the colon and then split at spaces
         let nicks = B.words $ B.drop (B.length chan + 2) $ snd $ B.breakSubstring chan $ mMsg m
+        -- Remove modifiers like @ for Op
         let newNicks = S.fromList $ map (\n -> if (elem . B.head) n "@+" then B.drop 1 n else n) nicks
+        -- Update nick list
         let newNickList = map (\(c,ns) -> if c == chan then (c, newNicks) else (c,ns)) nickList
         writeIORef ionicklist newNickList
         putStrLn $ show newNickList
         return ()
+      -- Ignore other messages
       else
-      return ()
+        return ()
     where chan = if isJust (mChan m) then fromJust (mChan m) else (mMsg m)
           nick = if isJust (mNick m) then fromJust (mNick m) else B.pack ""
+
+-- The main function
 main :: IO (Either IOError MIrc)
 main =
     do
+      -- Persistant memory to forward messages
       messages <- newIORef ([])
+      -- Same for a list of nicks in a channel
       nickList <- newIORef (map (\c -> (B.pack c,S.empty)) channelNames )
+      -- File handle for a possible log
       let fileName = logName
       fhandle <- if logging then openFile fileName AppendMode else return stderr
       handle <- newIORef $ fhandle
+      -- Register event handlers
       let events    = [(Privmsg (onPrivMsg messages nickList)), (Join (onJoinMsg messages nickList)), (Invite (onInviteMsg nickList)), (Part (onPartMsg nickList)), (RawMsg (onRawMsg handle nickList))]
+      -- General config
       let config    = (mkDefaultConfig "" "") {
           cAddr                = "chat.freenode.net",
           cPort                = 6667,
@@ -206,6 +250,7 @@ main =
           cCTCPVersion         = "Boten-Anna " ++ version
 --          cCTCPTime            = fmap (formatTime defaultTimeLocale "%c") getZonedTime
        }
+      -- Connect to server
       info <- connect config False debugMsg
       server <- let (Right l) = info in return l
       return info
