ef101cab232a68d47fd1c3285374c38179b42bf0
Herbert Lange <sonofchaos@gmx.de>
2016-03-17 14:38:18 +0100

    build gf grammar automatically

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,6 @@
 import Distribution.Simple
-main = defaultMain
+import System.Process
+
+main = defaultMainWithHooks simpleUserHooks{ postBuild = gfPostBuild }
+  where
+    gfPostBuild _ _ _ _ = callCommand "gf -make AnnaEngQ.gf AnnaEngR.gf AnnaAct.gf"
